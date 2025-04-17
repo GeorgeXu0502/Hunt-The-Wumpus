f16737531e514fb4b9ed38c4eef5b5a34fe85049
SergeiMakarevich <1117057@apps.nsd.org>
2025-04-17 15:01:59 -0700

    Added all the Final Game Folders

---
+++ Cave_Main_Team1/Form1.cs
@@ -0,0 +1,10 @@
+namespace Cave_Main_Team1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ PlayerInventory_Main_Team1/Form1.cs
@@ -0,0 +1,10 @@
+namespace PlayerInventory_Main_Team1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
