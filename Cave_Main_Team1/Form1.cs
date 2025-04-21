6025ab498c73caf844c7c9c10aa3e1027c04c001
anastasiamakarev <1117058@apps.nsd.org>
2025-04-21 15:02:08 -0700

    LEts try this again

--- Cave_Main_Team1/Form1.cs
+++ Cave_Main_Team1/Form1.cs
@@ -5,7 +5,7 @@ namespace Cave_Main_Team1
         public Form1()
         {
             InitializeComponent();
-            Console.WriteLine("testing hiii");
+            Console.WriteLine("Here lets go again");
         }
     }
 }
