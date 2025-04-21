ea09412542fc89217c65f8f12d0d24f18038abf0
SergeiMakarevich <1117057@apps.nsd.org>
2025-04-21 15:06:49 -0700

    Added in the new constructs for the main file. UI Based.

--- HuntTheWumpus_Team1/Form1.cs
+++ HuntTheWumpus_Team1/Form1.cs
@@ -1,3 +1,8 @@
+using Cave_Main_Team1;
+using GameControl_Main_Team1;
+using PlayerInventory_Main_Team1;
+using TriviaMachine_Main_Team1;
+
 namespace HuntTheWumpus_Team1
 {
     public partial class Form1 : Form
@@ -5,6 +10,11 @@ namespace HuntTheWumpus_Team1
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public void UserMovesForward()
+        {
+
         }
 
 
