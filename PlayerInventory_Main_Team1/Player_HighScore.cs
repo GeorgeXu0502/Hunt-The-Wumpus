426add2df4f220525b1d1cab546d1e51ea631663
2008158 <2008158@IHS6021900280>
2025-04-21 15:00:46 -0700

    Updated changes to the Wumpus Player & HighScore Code - George Xu

---
+++ PlayerInventory_Main_Team1/Player_HighScore.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayerInventory_Main_Team1
+{
+    public class Player_HighScore
+    {
+
+        public Player_HighScore()
+        {
+
+        }
+    }
+}
