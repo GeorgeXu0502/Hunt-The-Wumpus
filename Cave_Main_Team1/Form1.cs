6bad363b81ac3020c36a70b5561711293b0c43f9
anastasiamakarev <1117058@apps.nsd.org>
2025-04-21 15:11:02 -0700

    testing

--- Cave_Main_Team1/Form1.cs
+++ Cave_Main_Team1/Form1.cs
@@ -5,7 +5,7 @@ namespace Cave_Main_Team1
         public Form1()
         {
             InitializeComponent();
-            Console.WriteLine("tes");
+            Console.WriteLine("testing");
            
         }
     }
