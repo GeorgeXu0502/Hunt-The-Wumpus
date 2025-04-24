28e6637857e5a88dda31d1893596388f2e996509
anastasiamakarev <1117058@apps.nsd.org>
2025-04-24 15:12:33 -0700

    some basic stuff

---
+++ Cave_Main_Team1/CaveMain_Test/Room.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaveMain_Test
+{
+    public class Room
+    {
+        public bool HasBats(int newRoom)
+        {
+            return false;
+        }
+        public bool HasWumpus(int newRoom)
+        {
+            return false;
+        }
+        public bool HasPit(int newRoom)
+        {
+            return false;
+        }
+
+       
+
+
+    }
+}
