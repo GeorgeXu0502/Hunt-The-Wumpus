d81a0ef7f05b98fa76eee3ebb0d515250536906a
SergeiMakarevich <1117057@apps.nsd.org>
2025-04-24 14:00:34 -0700

    Added the Player_TriviaMachine Class

---
+++ TriviaMachine_Main_Team1/Player_TriviaMachine.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriviaMachine_Main_Team1
+{
+    public class Player_TriviaMachine
+    {
+
+    }
+}
