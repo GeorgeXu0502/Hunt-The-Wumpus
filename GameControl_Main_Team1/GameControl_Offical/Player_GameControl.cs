815fb8fb089d8552d636119a432d1f05057c4a0c
Sergei <1117057@apps.nsd.org>
2025-04-25 16:40:36 -0700

    Adding the PlayerObject

--- GameControl_Main_Team1/GameControl_Offical/Player_GameControl.cs
+++ GameControl_Main_Team1/GameControl_Offical/Player_GameControl.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TrivaMachine_Offical;
+using CaveMain_Offical;
 
 namespace GameControl_Offical
 {
@@ -11,6 +12,7 @@ namespace GameControl_Offical
     public class Player_GameControl
     {
         Player_TriviaMachine TriviaMachineObject = new TrivaMachine_Offical.Player_TriviaMachine();
+        Player_CaveMain CaveMainObject = new CaveMain_Offical.Player_CaveMain();
 
     }
 }
