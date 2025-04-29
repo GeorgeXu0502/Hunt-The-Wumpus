4441e66b0caeb9d086d29e08ca4cb8481605a7e9
SergeiMakarevich <1117057@apps.nsd.org>
2025-04-29 09:39:57 -0700

    Update Room.cs
    Added a Room Number Object.

--- Cave_Main_Team1/CaveMain_Offical/Room.cs
+++ Cave_Main_Team1/CaveMain_Offical/Room.cs
@@ -8,14 +8,16 @@ namespace CaveMain_Offical
 {
     public class Room
     {
+        public bool RoomNumber {get; set;}
         public bool HasBats { get; set; }
         public bool HasWumpus { get; set; }
         public bool HasPit { get; set; }
 
         public int[] NextRoomList { get; set; }
 
-        public Room(bool hasBats, bool hasWumpus, bool hasPit, int[] nextRoomList)
+        public Room(int roomnumber, bool hasBats, bool hasWumpus, bool hasPit, int[] nextRoomList)
         {
+            RoomNumber = roomnumber
             HasBats = hasBats;
             HasWumpus = hasWumpus;
             HasPit = hasPit;
