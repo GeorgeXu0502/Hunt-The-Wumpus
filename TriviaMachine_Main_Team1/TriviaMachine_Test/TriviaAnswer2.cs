9bd3f2ab4cae9ea6f423e9163dbbd1d59934721a
AdahGoel8 <1118271@apps.nsd.org>
2025-04-29 15:02:35 -0700

    Adding test stuff

---
+++ TriviaMachine_Main_Team1/TriviaMachine_Test/TriviaAnswer2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriviaMachine_Test
+{
+    public class TriviaAnswer2
+    {
+        public string TriviaQuestion { get; set; }
+
+        public string CorrectAnswer { get; set; }
+
+        public TriviaAnswer2(string triviaQuestion, string correctAnswer)
+        {
+            TriviaQuestion = triviaQuestion;
+            CorrectAnswer = correctAnswer;
+        }
+    }
+}
