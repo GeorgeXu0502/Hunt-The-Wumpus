a98f43b02ab1a4c7f6b1c5d046acde4f7e0810a0
SergeiMakarevich <1117057@apps.nsd.org>
2025-05-01 15:03:20 -0700

    Adding some Trivia Realted Items.

---
+++ TriviaMachine_Main_Team1/TrivaMachine_Offical/Utility.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace TrivaMachine_Offical
+{
+    public class Utility
+    {
+        public static void WritetoFile(List<TriviaQuestion> ListofTriviaQuestion, string filename)
+        {
+            JsonSerializer serlizer = new JsonSerializer();
+
+            serlizer.Formatting = Formatting.Indented;
+
+            StreamWriter streakwriter = new StreamWriter(filename);
+            serlizer.Serialize(streakwriter, ListofTriviaQuestion, typeof(List<TriviaQuestion>));
+
+            streakwriter.Flush();
+            streakwriter.Close();
+        }
+
+        public static List<TriviaQuestion> Readfromfile(string filename)
+        {
+            JsonSerializer serlizer = new JsonSerializer();
+            StreamReader streamReader = new StreamReader(filename);
+
+
+            var objectdefied = serlizer.Deserialize(streamReader, typeof(List<TriviaQuestion>));
+            streamReader.Close();
+            if (objectdefied != null)
+            {
+                return (List<TriviaQuestion>)objectdefied;
+            }
+            else
+            {
+                List<TriviaQuestion> newTriviaQuestionList = new List<TriviaQuestion>();
+                return newTriviaQuestionList;
+            }
+        }
+    }
+}
